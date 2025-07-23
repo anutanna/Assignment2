adbad638a7962ff3bda4cb088fb56b51097cdf4e
anutanna <atanna031@gmail.com>
2025-07-23 16:37:17 -0300

    fix2

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 // app/page.tsx
-import LatestProducts from "@/lib/ui/pages/home/LatestProductsSection";
-import PromotionsBanner  from "@/lib/ui/pages/home/PromoBannerSection";
+export const dynamic = 'force-dynamic';
 
+import LatestProducts from "@/lib/ui/pages/home/LatestProductsSection";
+import PromotionsBanner from "@/lib/ui/pages/home/PromoBannerSection";
 
 export default function Home() {
   return (
     <div className="page">
-     
       <main>
         <PromotionsBanner />
-       
         <LatestProducts />
       </main>
-
-      
     </div>
   );
 }
